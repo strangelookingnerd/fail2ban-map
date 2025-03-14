cc2ca72dc0fbfaa8029b0783e9355fa61c704757
strangelookingnerd <49242855+strangelookingnerd@users.noreply.github.com>
2025-03-14 23:00:48 +0100

    Finalize

--- public/fail2ban-map.js
+++ public/fail2ban-map.js
@@ -33,8 +33,16 @@ window.onload = async function () {
   // list of tile providers can be seen here: https://leaflet-extras.github.io/leaflet-providers/preview/
   L.tileLayer.provider("CartoDB.DarkMatterNoLabels").addTo(map);
 
-  const info = L.control.info();
+  const info = L.control.info({
+    title: `<i style="color: rgb(69, 69, 69)" class="fa-solid fa-circle-info"></i>&nbsp;Info`,
+  });
   const markersLayer = L.geoJson(null);
+  const marker = L.ExtraMarkers.icon({
+    icon: "fa-solid fa-ban",
+    markerColor: "red",
+    shape: "circle",
+    svg: "true",
+  });
   const heatmapLayer = L.heatLayer([], {
     radius: 35,
     gradient: {
@@ -57,31 +65,21 @@ window.onload = async function () {
     const response = await fetch("places.geojson");
     const data = await response.json();
 
-    info.setTitle(`
-    <i style="color: rgb(69, 69, 69)" class="fa-solid fa-circle-info"></i>&nbsp;Info`);
-
     info.setContent(`
+    <a class="github-fork-ribbon" target="_blank" href="https://github.com/strangelookingnerd/fail2ban-map" 
+       data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>
     <div class="info-container">
         <img src="./favicon.ico" alt="Icon">
         <div class="banned-count">${data.features.length} banned IP</div>
-    </div>
-    <a class="github-fork-ribbon" target="_blank" href="https://github.com/strangelookingnerd/fail2ban-map" 
-       data-ribbon="Fork me on GitHub" title="Fork me on GitHub">Fork me on GitHub</a>`);
+    </div>`);
 
     data.features.forEach((feature) => {
       const { properties, geometry } = feature;
       if (!properties?.show_on_map) return;
 
-      const icon = L.ExtraMarkers.icon({
-        icon: "fa-solid fa-ban",
-        markerColor: "red",
-        shape: "circle",
-        svg: "true",
-      });
-
       const layer = L.geoJSON(feature, {
         pointToLayer: (feature, latlng) => {
-          return L.marker(latlng, { icon: icon });
+          return L.marker(latlng, { icon: marker });
         },
         onEachFeature: (feature, layer) => {
           if (properties?.place) {
